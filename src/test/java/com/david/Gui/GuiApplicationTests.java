fa546fd9c6dbf06a26250cbf0ed9601421b4aef2
David Raysor <69765212+DaveR0629@users.noreply.github.com>
2021-02-09 16:31:00 -0500

    Gui

---
+++ src/test/java/com/david/Gui/GuiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.david.Gui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
